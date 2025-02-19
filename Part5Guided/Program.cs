e77ca18be5630af5fb226e1589894ed9c05e86b8
Acamerus <111820648+Acamerus@users.noreply.github.com>
2025-02-19 03:23:43 -0500

    Creation of 2D array [6,3] to handle groups and animals Unit 5

--- Part5Guided/Program.cs
+++ Part5Guided/Program.cs
@@ -7,25 +7,38 @@ string[] pettingZoo =
     "ostriches", "pigs", "ponies", "rabbits", "sheep", "tortoises",
 };
 
-// RandomizeAnimals();
 RandomizeAnimals();
 
-// string[,] group = AssignGroup(); 
+string[,] group = AssignGroup();
 
 Console.WriteLine("School A");
 Console.WriteLine("School B");
 Console.WriteLine("School C");
 
-// PrintGroup(group);
+PrintGroup(group);
+
+// 2D Array 6 groups of 3 animals
+string[,] AssignGroup(int groups = 6) 
+{
+    string[,] result = new string[groups, pettingZoo.Length/groups];
+    int start = 0;
+    
+    for (int i = 0; i < groups; i++) 
+    {
+        for (int j = 0; j < result.GetLength(1); j++) 
+        {
+            result[i,j] = pettingZoo[start++];
+        }
+    }
+    return result;
+}
 
 void RandomizeAnimals() 
 {
     Random random = new Random();
-
     for (int i = 0; i < pettingZoo.Length; i++) 
     {
         int r = random.Next(i, pettingZoo.Length);
-
         string temp = pettingZoo[i];
         pettingZoo[i] = pettingZoo[r];
         pettingZoo[r] = temp;
@@ -36,3 +49,16 @@ void RandomizeAnimals()
 // {
 //     Console.WriteLine(animal);
 // }
+
+void PrintGroup(string[,] groups)
+{
+    for (int i = 0; i < groups.GetLength(0); i++)
+    {
+        Console.Write($"Group {i + 1}: ");
+        for (int j = 0; j < groups.GetLength(1); j++)
+        {
+            Console.Write($"{groups[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
